f0d306b51896c55cf1a55083a7c53b52869335de
dbstjddbwls <dbstjddbwls@naver.com>
2022-06-14 13:14:13 +0900

    userSlice refeact

--- src/reducer/userClipSlice.ts
+++ src/reducer/userClipSlice.ts
@@ -1,21 +1,31 @@
 import { persistReducer } from "redux-persist";
-import { createSlice } from "@reduxjs/toolkit";
+
+import { createSlice, current } from "@reduxjs/toolkit";
+import { act } from "react-dom/test-utils";
 
 export const userClipSlice = createSlice({
   name: "userClip",
   initialState: {
-    content: [{ content: "", id: "", title: "", contents:"", clip: false }],
+    content: [{ id: "", title: "", abstract: "", clip: null, test: "test" }],
   },
   reducers: {
-    clipNews: (state:any, action:any):any => {
-      console.log(state);
-      console.log("클립뉴스 함수로 들어왔어요");
+    clipNews: (state, action): any => {
+      // console.log(current(state.content));
+      console.log(current(state));
       console.log(action.payload);
+      console.log("클립뉴스 함수로 들어왔어요");
       state.content.push({ ...action.payload, clip: true });
     },
-    unclipNews: (state:any, action:any):any => {
-      console.log(action.payload);
-      return { ...action.payload, clip: false };
+    unclipNews: (state, action): any => {
+      console.log("언클립 뉴스 함스로 들어왔어요");
+      // console.log(action.payload);
+      // state.content = { ...action.payload, clip: false };
+
+      return {
+        content: state.content.filter(
+          (content) => content.id !== action.payload.id
+        ),
+      };
     },
   },
 });
