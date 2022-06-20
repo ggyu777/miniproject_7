053d52e47f7238271e7b608987ed864ee3a5d588
현병택 <hyeonbyeongtaeg@hyeonbyeongtaegs-MacBook-Air.local>
2022-06-20 13:47:25 +0900

    Clip type fixed

--- src/reducer/userClipSlice.ts
+++ src/reducer/userClipSlice.ts
@@ -5,17 +5,18 @@ import { act } from "react-dom/test-utils";
 export const userClipSlice = createSlice({
   name: "userClip",
   initialState: {
-    content: [{ id: "", title: "", name:"", date:"", content: "", clip: false, url: "" }],
+    content: [{ id: "", title: "", name:"", date:"", content:"", clip: false, url: "" }],
   },
   // 클립뉴스 리듀서 : 클립된 컨텐트 state에 넣어주기
   reducers: {
-    clipNews: (state, action): any => {
+    clipNews: (state, action) => {
       state.content.push({ ...action.payload, clip: true });
     },
 
+    //clipNews : InewsSearch
     // 언클립뉴스 리듀서: 클릭된 뉴스 없애주기
   // });
-    unclipNews: (state, action): any|undefined => {
+    unclipNews: (state, action) => {
       let findeNewsNum = state.content.findIndex((a)=>{return (a.id == action.payload ? a : null)})
       state.content.splice(findeNewsNum,1)
     },
